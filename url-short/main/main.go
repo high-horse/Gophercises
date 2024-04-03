ceea35d0dc14b4ca874b13cd322a4f31e8c40f21
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-04-03 12:46:28 +0545

    use flag and yaml file for external urls

--- url-short/main/main.go
+++ url-short/main/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	urlshort "url-short"
+
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
+	ymlFile := flag.String("yml" , "", "pass yaml file name")
+	flag.Parse()
+
+	parsed_yaml, err := ParseYaml(*ymlFile)
+	if err != nil {
+		log.Fatal("Error parsing yml file: ", err)
+		return
+	}
+
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -18,12 +32,22 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
+	var yaml string
+	if(len(parsed_yaml) < 1) {
+		yaml = `
+			- path: /urlshort
+			url: https://github.com/gophercises/urlshort
+			- path: /urlshort-final
+			url: https://github.com/gophercises/urlshort/tree/solution
+		`
+	} else {
+		yaml, err = GenerateYMLString(parsed_yaml)
+		if err != nil {
+			log.Fatal("Error parsing yml file: ", err)
+			return
+		}
+	}
+	
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
 	if err != nil {
 		panic(err)
@@ -40,4 +64,36 @@ func defaultMux() *http.ServeMux {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
+}
+
+
+type URLmap struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
+func ParseYaml(filename string) ([]URLmap, error) {
+	var mapped_data []URLmap
+	// var mapped_data map[string]string
+
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	err  = yaml.Unmarshal(data, &mapped_data)
+	if err != nil {
+		return nil, err
+	}
+	return mapped_data, nil
+}
+
+func GenerateYMLString(data []URLmap) (string, error) {
+	yamlBytes, err := yaml.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+	return string(yamlBytes), nil
+
 }
