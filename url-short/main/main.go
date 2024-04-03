524d1d02248ee92566b5ffa024a48cd381006c52
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-04-03 15:35:36 +0545

    make flag optional

--- url-short/main/main.go
+++ url-short/main/main.go
@@ -6,54 +6,46 @@ import (
 	"log"
 	"net/http"
 	"os"
-	urlshort "url-short"
 
-	"gopkg.in/yaml.v2"
+	urlshort "url-short"
 )
 
 func main() {
-	ymlFile := flag.String("yml" , "", "pass yaml file name")
+	ymlFile := flag.String("yml", "", "pass yaml file name")
 	flag.Parse()
 
-	parsed_yaml, err := ParseYaml(*ymlFile)
-	if err != nil {
-		log.Fatal("Error parsing yml file: ", err)
-		return
+	var parsedYAML []urlshort.PathURL
+	var err error
+
+	if *ymlFile != "" {
+		data, err := os.ReadFile(*ymlFile)
+		if err != nil {
+			log.Fatalf("Error reading yml file: %v", err)
+		}
+		parsedYAML, err = urlshort.ParseYAML(data)
+		if err != nil {
+			log.Fatalf("Error parsing yml file: %v", err)
+		}
+	} else {
+		// Use the default YAML string
+		defaultYAML := []byte(`
+            - path: /urlshort
+              url: https://github.com/gophercises/urlshort
+            - path: /urlshort-final
+              url: https://github.com/gophercises/urlshort/tree/solution
+        `)
+		parsedYAML, err = urlshort.ParseYAML(defaultYAML)
+		if err != nil {
+			log.Fatalf("Error parsing default YAML: %v", err)
+		}
 	}
 
 	mux := defaultMux()
-
-	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
-	}
+	pathsToUrls := urlshort.BuildMap(parsedYAML)
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	var yaml string
-	if(len(parsed_yaml) < 1) {
-		yaml = `
-			- path: /urlshort
-			url: https://github.com/gophercises/urlshort
-			- path: /urlshort-final
-			url: https://github.com/gophercises/urlshort/tree/solution
-		`
-	} else {
-		yaml, err = GenerateYMLString(parsed_yaml)
-		if err != nil {
-			log.Fatal("Error parsing yml file: ", err)
-			return
-		}
-	}
-	
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
@@ -64,36 +56,4 @@ func defaultMux() *http.ServeMux {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
-}
-
-
-type URLmap struct {
-	Path string `yaml:"path"`
-	URL  string `yaml:"url"`
-}
-
-func ParseYaml(filename string) ([]URLmap, error) {
-	var mapped_data []URLmap
-	// var mapped_data map[string]string
-
-
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	err  = yaml.Unmarshal(data, &mapped_data)
-	if err != nil {
-		return nil, err
-	}
-	return mapped_data, nil
-}
-
-func GenerateYMLString(data []URLmap) (string, error) {
-	yamlBytes, err := yaml.Marshal(data)
-	if err != nil {
-		return "", err
-	}
-	return string(yamlBytes), nil
-
 }
