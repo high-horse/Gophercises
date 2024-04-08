e9daa1b25c15c56f66c80708594e4f7279056497
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2024-04-08 23:02:44 +0545

    passing url cmd param

--- HTML-Link-Parser/main.go
+++ HTML-Link-Parser/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"html-link-parser/parser"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -22,26 +24,27 @@ var exHtml = `
 
 func main() {
 	filename := flag.String("file", "", "fle to be parsed")
+	url := flag.String("url", "", "URL to be parsed")
 	flag.Parse()
-	
+
 	var text string
-	if *filename == "" {
-		text = exHtml
-	} else {
+	if *url != "" {
+		var err error
+		text, err = getHtml(*url)
+		println(text)
+		checkErr(err, "getting html data")
+	} else if *filename != "" {
 		var err error
 		text, err = readFromFile(*filename)
-		if err != nil{
-			os.Exit(1)
-		}
+		checkErr(err, "reading file contents")
+	} else {
+		text = exHtml
 	}
 	r := strings.NewReader(text)
 	links, err := parser.Parse(r)
-	if err != nil {
-		panic(err)
-	}
+	checkErr(err, "parse file and generate links")
 	fmt.Printf("%+v\n", links)
 }
-
 
 func readFromFile(filename string) (string, error) {
 	bytes, err := os.ReadFile(filename)
@@ -49,4 +52,29 @@ func readFromFile(filename string) (string, error) {
 		return "", err
 	}
 	return string(bytes), nil
+}
+
+func getHtml(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", nil
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusForbidden {
+		return "", fmt.Errorf("403 Forbidden: Access to %s is not allowed", url)
+	}
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", nil
+	}
+	return string(bytes), nil
+}
+
+func checkErr(err error, message string) {
+	if err != nil {
+		fmt.Printf("%s: %v\n", message, err)
+		os.Exit(1)
+	}
 }
