5d7749af0e43f800dd1f111d9797b0fc55209385
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2024-06-25 08:37:48 +0545

    create bucket

--- 07-CLI-Task-Manager/db/bucket.go
+++ 07-CLI-Task-Manager/db/bucket.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -25,4 +26,32 @@ func Init(dbPath string) error {
 		_, err := tx.CreateBucketIfNotExists(taskBucket)
 		return err
 	})
+}
+
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(taskBucket)
+		id64, _ := bucket.NextSequence()
+		id = int(id64)
+		key := itob(id)
+		return bucket.Put(key, []byte(task))
+		// return bucket.Put(itob(id64), []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+
+	return id, nil
+}
+
+func itob(id int) []byte {
+	b := make([]byte, 8)
+	// binary.BigEndian.PutUint64(b, id)
+	binary.BigEndian.PutUint64(b, uint64(id))
+	return b
+}
+
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
 }
