e8beea5661787836f9e4d95854e76319054e145b
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2024-06-25 08:24:51 +0545

    db connection success

---
+++ 07-CLI-Task-Manager/db/bucket.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+var taskBucket = []byte("tasks")
+var db *bolt.DB
+
+type Task struct {
+	Key   int
+	Value string
+}
+
+func Init(dbPath string) error {
+	var err error
+	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	
+	return db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(taskBucket)
+		return err
+	})
+}
--- 07-CLI-Task-Manager/main.go
+++ 07-CLI-Task-Manager/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "cli-task/cmd"
+import (
+	"cli-task/cmd"
+	"cli-task/db"
+	"fmt"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
+	home, err := homedir.Dir()  
+	if err != nil {
+		panic(err)
+	}
+	dbPath  := filepath.Join(home, "tasks.db")
+	err = db.Init(dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	// defer db.Close()
+	fmt.Println("db success")
+
 	cmd.RootCmd.Execute()
 }
