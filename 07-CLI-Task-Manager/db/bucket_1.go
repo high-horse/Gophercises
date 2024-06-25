c669ce3654c648e9dba7c1bf4eebbba2abf539a2
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-06-25 16:34:53 +0545

    done kindof

---
+++ 07-CLI-Task-Manager/db/bucket_1.go
@@ -0,0 +1,132 @@
+package db
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+type Work struct {
+	ID          int
+	Name        string
+	Completed   bool
+	CompletedAt time.Time
+}
+
+var bucket_name = []byte("work")
+var bucket *bolt.DB
+
+func InitBucket() {
+	var err error
+	db, err = bolt.Open("my.db", 0600,  &bolt.Options{Timeout: 1 * time.Second})
+
+	if err != nil {
+		panic(err)
+	}
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(bucket_name)
+		return err
+	})
+}
+
+func CreateWork (task string) (int, error) {
+	var id int
+	work := Work{
+		Name: task,
+		Completed: false,
+	}
+	err := db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(bucket_name)
+		id64, _ := bucket.NextSequence()
+		id = int(id64)
+		key := itob(id)
+
+		workBytes, err := json.Marshal(work)
+		if err != nil {
+			return err
+		}
+		return bucket.Put(key, workBytes)
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+
+func ListWork() ([]Work, error) {
+	var works []Work
+	err := db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(bucket_name)
+		c := bucket.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			var work Work
+			err := json.Unmarshal(v, &work)
+			if err != nil {
+				return err
+			}
+			works = append(works, work)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return works, nil
+}
+
+
+func DeleteWork(key int) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(bucket_name)
+		return bucket.Delete(itob(key))
+	})
+}
+
+func UpdateWork(key int, status bool) error {
+	var work *Work
+	var err error
+
+	work, err = GetWork(key)
+
+	if err != nil {
+		return err
+	}
+
+	err =  db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(bucket_name)
+
+		// work.ID = key
+		work.Completed = status
+		work.CompletedAt = time.Now()
+
+		workBytes, err := json.Marshal(work)
+		if err != nil {
+			return err
+		}
+		return bucket.Put(itob(key), workBytes)
+	})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetWork(key int) (*Work, error) {
+	var work *Work
+	err := db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(bucket_name)
+		v := bucket.Get(itob(key))
+		err := json.Unmarshal(v, &work)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return work, nil
+}
--- 07-CLI-Task-Manager/go.mod
+++ 07-CLI-Task-Manager/go.mod
@@ -2,12 +2,14 @@ module cli-task
 
 go 1.22.4
 
-require github.com/spf13/cobra v1.8.1
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.8.1
+)
 
 require (
-	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 )
